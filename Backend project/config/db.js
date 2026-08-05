415e0cbb5c1a424a82d25de8c7aa29bda879cae6
Geethanjali <munnyvantla@gmail.com>
2026-08-05 10:49:54 +0530

    Updated database configuration

--- Backend project/config/db.js
+++ Backend project/config/db.js
@@ -7,7 +7,7 @@ const db = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 15,
     queueLimit: 0
 });
 
